0288211b87b30099d8de58395cf3b1438fa844e5
jrose0116 <jrose0116@gmail.com>
2023-05-29 17:51:04 -0400

    refactor public to docs

--- index.js
+++ index.js
@@ -1,21 +1,25 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
 const app = express();
 
 //automatically render index.html and allow other html pages in path
-app.use('/flowbite', express.static(path.join(__dirname,'node_modules/flowbite/dist')))
+app.use(
+  "/flowbite",
+  express.static(path.join(__dirname, "node_modules/flowbite/dist"))
+);
 
-app.use('/', express.static('public',{extensions: ['html']}));
-
+app.use("/", express.static("docs", { extensions: ["html"] }));
 
 //if not found, send back to index
-app.get('*', (req, res) => { res.redirect('/') })
+app.get("*", (req, res) => {
+  res.redirect("/");
+});
 
 //find port automatically or 3000 if local
-const port = process.env.PORT||3000;
+const port = process.env.PORT || 3000;
 
 //listens for http requests using app
-app.listen(port, ()=>{
-    console.log("Connected...")
+app.listen(port, () => {
+  console.log("Connected...");
 });
